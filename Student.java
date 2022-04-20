157300768996a074b076581f2de5abb9a787c6af
raaaimund <raimund.rittnauer@gmail.com>
2022-04-20 14:58:23 +0530

    added Student java file

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    private final String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
