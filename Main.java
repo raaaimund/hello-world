1c9ef6277984ee6eea06ead111fa0257958a22c5
raaaimund <raimund.rittnauer@gmail.com>
2022-04-20 14:54:50 +0530

    changed output for assignment 2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello CHANGE IMPLEMENTATION");
+        System.out.println("Changes for assignment 2");
     }
 }
